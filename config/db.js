a3593aea239d7fcf94a4e6dc5ae20310bda89647
Yan Waipann <yanwaipann@gmail.com>
2023-07-02 18:43:15 +0630

    chore: update deps and address deprecation warnings

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 // @desc    Connects to mongodb and logs hostname
 const connectDB = async () => {
+  mongoose.set("strictQuery", false);
   const conn = await mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
